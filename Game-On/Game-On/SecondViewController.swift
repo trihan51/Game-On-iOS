f3a50e86c25fabad17d1cf59d824375bd0f4e628
ManbirRandhawa <mrandhawa27@gmail.com>
2016-03-21 14:26:09 -0700

    Basic structure
    No functionality yet, just splash page, login page, register page,
    cancel buttons work nothing else does

---
+++ Game-On/Game-On/SecondViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.swift
+//  Game-On
+//
+//  Created by Manbir Randhawa on 3/21/16.
+//  Copyright © 2016 GameOn. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController{
+    
+    
+    
+}
---
+++ Game-On/Game-On/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Game-On
+//
+//  Created by Manbir Randhawa on 3/21/16.
+//  Copyright © 2016 GameOn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+}
+
